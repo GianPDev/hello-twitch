4c0776fabd891f65530131605495c9ee0a6c15b4
Gian Prescilla <gianpdev@gmail.com>
2021-09-04 01:25:04 +1000

    Added Jisho dictionary, started LibreTranslate.

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,15 @@ use twitch_irc::message::{IRCMessage, ServerMessage};
 use chrono::{Datelike, Timelike, Utc};
 use std::sync::Arc;
 use std::borrow::Cow;
+use libretranslate::{translate_url, Language};
+use jisho::lookup;
 
 //TODO use std::in for entering botname and multiple channels, then parse multiple channel joining, using substrings and do a for loop on all of the channel names
 
 #[tokio::main]
 pub async fn main() {
   dotenv().ok();
+  let target_lang = Language::English;
   //let now = Utc::now();
   //let(is_pm, hour) = now.hour12();
   // let bot_name_str = Arc::new(String::from("zowlbot").to_lowercase()); //Change to reading from a file or UI Input
@@ -25,6 +28,7 @@ pub async fn main() {
   let bot_name_str = Cow::from(String::from("zowlbot").to_lowercase()); //Change to reading from a file or UI Input
   // let bot_name = &bot_name_str;
   let oauth_token_str = env::var("TWITCH_OAUTH").unwrap().to_string();
+  let translation_server = Cow::from(String::from(env::var("TRANSLATE_SERVER").unwrap()));
   // let channel_name_str = Arc::new(String::from("devizowl").to_lowercase());
   let mut channels: Vec<String> = vec![String::new(); 1];
   channels[0] = "devizowl".to_string();
@@ -55,7 +59,7 @@ pub async fn main() {
           println!("[{:02}:{:02}](#{}) {}: {}", hour, now.minute(), msg.channel_login, msg.sender.name, msg.message_text);
           let text = msg.message_text;
           let sender = msg.sender.name;
-          let reply = format!("Are you scared >>>{}<<<? :)", sender.to_string());
+          // let reply = format!("Are you scared >>>{}<<<? :)", sender.to_string());
           // let bot_name = login_name.to_lowercase().to_string();
           // let bot_name = &bot_name_str;
           // println!("botname: {}", bot_name);
@@ -132,6 +136,82 @@ pub async fn main() {
                 
                   println!("[{:02}:{:02}]{}", hour, now.minute(), reading_form);
                   client.send_message(twitch_irc::message::IRCMessage::new_simple("PRIVMSG".to_string(), vec![format!("#{}", msg.channel_login), reading_form.to_string()])).await.unwrap();
+                } //end of !jpd
+                
+                //libre translate server downloading too many languages and this function not working properly
+                //thread 'tokio-runtime-worker' panicked at 'called `Result::unwrap()` on an `Err` value: RelativeUrlWithoutBase', C:\Users\TFI3080\.cargo\registry\src\github.com-1ecc6299db9ec823\surf-2.3.1\src\one_off.rs:131:36
+                // note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+                // thread 'main' panicked at 'called `Result::unwrap()` on an `Err` value: JoinError::Panic(...)', src\main.rs:241:21
+                /* "!lt" | "!LT" => {
+                  println!("Do libretranslate with: {}", value);
+                  let data = translate_url(Language::Detect, target_lang, value, translation_server.to_string(), None).await;
+                  if data.is_ok() {
+                    let ok_data = data.unwrap();
+                    println!("LT: {} | {}", ok_data.target.as_pretty(), ok_data.output);
+                  }
+                  else if data.is_err() {
+                    let err = data.err().unwrap();
+                    println!("err {}", err);
+                    // println!("LT: {} | {}", data.target.as_pretty(), data.output);
+                  }
+                  // let output = match data {
+                  //   Ok(v) => {println("{}", v)},
+                  //   Err(e) => {println!("{}", e)},
+                  // }
+                  // println!("LT: {} | {}", data.target.as_pretty(), data.output);
+                } //end of !lt
+ */
+                "!jisho" | "!Jisho" => {
+                  let entries = lookup(value.as_str());
+                  let mut reading_form = String::from(&value);
+                  for entry in entries {
+                    // reading_form.push_str(entry.kanji.as_str());
+                    reading_form.push_str("「");
+                    reading_form.push_str(entry.reading.as_str());
+                    reading_form.push_str("」>>");
+                    for meaning in &entry.meanings
+                    { 
+                      reading_form.push_str(" | ");
+                      reading_form.push_str(meaning);
+                    }
+                  }
+                  reading_form.push_str(" | << [for ");
+                  reading_form.push_str(sender.as_str());
+                  reading_form.push_str("]");
+
+                  //split the string into parts so that it can paste the whole meaning
+                  //get length if certain length substrin and send twice with delay
+                  println!("({})Jisho: {} | << [for {}]", reading_form.len(),reading_form, sender);
+                  //this was for when the string goes over 50 lines which is the limit for non mods
+                  // let str_length = reading_form.len();
+                  // if str_length > 49 {
+                  //   println!("more than 50 characters");
+                  //   let mut index = 0;
+                    
+                  //   while index < str_length-1 {
+                  //     let mut slice = "";
+                  //     println!("Index: {}", index); 
+                  //     if index >= str_length-1 {
+                  //       let sn = str_length - index -1;
+                  //       slice = &reading_form[sn..];
+                  //     } else {
+                  //       slice = &reading_form[index..];
+                  //     }
+                  //     //let slice2 = &reading_form[49..];
+                  //     println!("Slice: {}", slice);
+                  //     client.send_message(twitch_irc::message::IRCMessage::new_simple("PRIVMSG".to_string(), vec![format!("#{}", msg.channel_login), slice.to_string()])).await.unwrap();
+                  //     //should put delay here                      
+                  //     //client.send_message(twitch_irc::message::IRCMessage::new_simple("PRIVMSG".to_string(), vec![format!("#{}", msg.channel_login), slice2.to_string()])).await.unwrap();
+                      
+                  //     if index + 50 >= str_length {
+                  //       index = str_length - 51;
+                  //     } else {
+                  //       index += 50;
+                  //     }
+                  //   }
+                  // } else {
+                    client.send_message(twitch_irc::message::IRCMessage::new_simple("PRIVMSG".to_string(), vec![format!("#{}", msg.channel_login), reading_form.to_string()])).await.unwrap();
+                  // }
                 }
                 _ => {println!("Invalid Command"); }
               }
@@ -146,8 +226,8 @@ pub async fn main() {
             }
             if text.contains("monkaS")  {
               println!("monkaS detected");
-              println!("{} @{}?", reply, sender);
-              client.send_message(twitch_irc::message::IRCMessage::new_simple("PRIVMSG".to_string(), vec![format!("#{}", msg.channel_login), reply])).await.unwrap();
+              // println!("{} @{}?", reply, sender);
+              // client.send_message(twitch_irc::message::IRCMessage::new_simple("PRIVMSG".to_string(), vec![format!("#{}", msg.channel_login), reply])).await.unwrap();
             }
           }
        },
